66a61bf4f1d2b5622a06340269db273bd8521a7d
Mohammad Fikri <fikri.mohammad30@gmail.com>
2020-01-25 16:26:18 +0700

    Add models and reports path

--- src/configs/path.py
+++ src/configs/path.py
@@ -2,3 +2,5 @@ import os
 
 RAW_DATA_PATH = os.path.realpath('data/raw')
 PROCESSED_DATA_PATH = os.path.realpath('data/processed')
+MODELS_PATH = os.path.realpath('models')
+REPORTS_PATH = os.path.realpath('reports')
